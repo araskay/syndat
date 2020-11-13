84fdbe16a262e076bd0bfba88d1659d4c4345ced
Aras Kayvanrad <mkayvan@gmail.com>
2020-11-13 11:47:49 -0500

    update plotter

--- plotter.py
+++ plotter.py
@@ -1,61 +1,60 @@
+import numpy as np
+import scipy.stats as ss
 import matplotlib.pyplot as plt
 from statsmodels.graphics.gofplots import qqplot_2samples
-import scipy
 import pandas as pd
 
-def compare_quant(df1, df2, col_name):
-    qqplot_2samples(
-        df1[col_name], df2[col_name],
-        xlabel='original data', ylabel='synthetic data', line='45'
-    )
-    plt.title(col_name)
-    plt.show()
-    # KS test
-    ks_stats, p = scipy.stats.ks_2samp(df1[col_name], df2[col_name])
-    print('KS p-value =', p)
-    
-def compare_dt(df1, df2, col_name, dt1_format=None, dt2_format=None):
-    df1_ord_dt = df1.copy()
-    df1_ord_dt[col_name] = (
-        pd.to_datetime(df1_ord_dt[col_name], format=dt1_format).
-        apply(lambda x: x.toordinal())
-    )
-    
-    df2_ord_dt = df2.copy()
-    df2_ord_dt[col_name] = (
-        pd.to_datetime(df2_ord_dt[col_name], format=dt2_format).
-        apply(lambda x: x.toordinal())
-    )
-    
-    compare_quant(df1_ord_dt, df2_ord_dt, col_name)
-    
-    
-def qq_categ(df1, df2, sd, col):
-    df1_le = sd.categ_to_label(df1, sd.cols)[0].copy()
-    df2_le = sd.categ_to_label(df2, sd.cols)[0].copy()
-    compare_quant(df1_le, df2_le, col)
-    
-    
-def compare_cat(df1, df2, col_name):
-    fig, ax = plt.subplots(1,2)
-    bar_dat = df1[col_name].value_counts().reset_index().sort_values('index')
-    ax[0].bar(bar_dat['index'],bar_dat[col_name])
-    ax[0].set_xticklabels(bar_dat['index'], rotation=90)
+def bhatta_coef(x1, x2, n_bins = 10):
+    cx = np.concatenate((x1, x2))
+    h1 = np.histogram(x1, bins = n_bins, range=(np.min(cx), np.max(cx)), density=True)[0]
+    h2 = np.histogram(x2, bins = n_bins, range=(np.min(cx), np.max(cx)), density=True)[0]
+    bhatta_coef = 0
+    for i in range(n_bins):
+        bhatta_coef += np.sqrt(h1[i]*h2[i]) * (np.max(cx)-np.min(cx))/n_bins
+    return bhatta_coef
 
-    bar_dat = df2[col_name].value_counts().reset_index().sort_values('index')
-    ax[1].bar(bar_dat['index'],bar_dat[col_name])
-    ax[1].set_xticklabels(bar_dat['index'], rotation=90)
-    fig.suptitle(col_name)
-    plt.show()
-    
-def comparison_plots(
-    df1, df2, cols, sd, dt1_format = None, dt2_format=None):
+def bhatta_dist(x1, x2, n_bins = 10):
+    return -np.log(bhatta_coef(x1, x2, n_bins=n_bins))
+
+def compare_hists(orig, syn, cols):
+    nbin = 50
     for c in cols:
-        if cols[c] == 'float' or cols[c] == 'int' or cols[c] == 'ord':
-            compare_quant(df1, df2, c)
-        if cols[c] == 'unord':
-            qq_categ(df1, df2, sd, c)
-        if cols[c] == 'dt':
-            compare_dt(
-                df1, df2, c, dt1_format=dt1_format, dt2_format=dt2_format
-            )
+        plt.figure()
+        _max = np.nanmax(np.concatenate((orig[c],syn[c])), )
+        _min = np.nanmin(np.concatenate((orig[c],syn[c])))
+        orig[c].hist(bins = nbin, alpha=0.5, range=(_min,_max), density=True, label='Original')
+        syn[c].hist(bins = nbin, alpha=0.5, range=(_min,_max), density=True, label='Synthetic')
+        
+        d = bhatta_coef(orig[c].to_numpy(), syn[c].to_numpy())
+        
+        plt.title(c+'\nBhattacharyya coef = {:.2f}'.format(d))
+        plt.legend()
+        plt.show()
+
+def compare_cdf(orig, syn, cols):
+    for c in cols:
+        plt.figure()
+        orig_sorted = np.sort(orig[c])
+        p_orig = 1.0 * np.arange(len(orig_sorted)) / (len(orig_sorted)-1)
+
+        syn_sorted = np.sort(syn[c])
+        p_syn = 1.0 * np.arange(len(syn_sorted)) / (len(syn_sorted)-1)
+
+        ks,p = ss.ks_2samp(syn[c], orig[c])
+        
+        plt.plot(orig_sorted, p_orig, label='Original')
+        plt.plot(syn_sorted, p_syn, label='Synthetic')
+        plt.title(c+'\nKS = {:.2f}, p = {:.2f}'.format(ks,p))
+        plt.legend()
+        plt.show()
+        
+def compare_qqplot(orig, syn, cols):
+    for c in cols:
+        plt.figure()
+        if len(syn[c]) < len(orig[c]):
+            qqplot_2samples(syn[c], orig[c])
+        else:
+            qqplot_2samples(orig[c], syn[c])
+        plt.title(c)
+        plt.show()
+
